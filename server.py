f07b3ebe61255012449a1f441abeaaf54dcb9d57
alf808 <alf@akademe.net>
2019-08-03 23:31:47 -1000

    added stdin input for testing

--- server.py
+++ server.py
@@ -3,23 +3,20 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 8888))
-sock.listen(1)
+sock.listen(2) # how many clients to listen to
+
+print('waiting for connection')
+conn, client_addr = sock.accept()
+print(f'connection from {client_addr}')
 
 while True:
-    print('waiting for connection')
-    conn, client_addr = sock.accept()
-    # from_client = b''
+    data = conn.recv(1024).decode() # decode byte from client
+    if data:
+        print('from client: ' + str(data))
+        data = input('=>=> ')
+        conn.send(data.encode()) # encode the string into byte for client
+    else:
+        break
 
-    try:
-        print(f'connection from {client_addr}')
-        while True:
-            data = conn.recv(4096)
-            print(f'received {data}')
-            if data:
-                print('hello client. you are receiving data from server')
-                # from_client += data
-                # print(from_client)
-                conn.send(data)
-    finally:
-        conn.close()
-        print('client left')
+conn.close()
+print('client left')
