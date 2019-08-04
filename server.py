8e93ed14b06874c30cce1831d5d3b689e8b39a01
alf808 <alf@akademe.net>
2019-08-04 08:41:47 -1000

    maintain server connection

--- server.py
+++ server.py
@@ -5,20 +5,29 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 8888))
 sock.listen(2) # how many clients to listen to
 
-print('waiting for connection')
-conn, client_addr = sock.accept()
-print(f'connection from {client_addr}')
-
+# maintains connection forever
 try:
     while True:
-        data = conn.recv(1024).decode() # decode byte from client
-        if data:
-            print('from client: ' + str(data))
-            data = input('=>=> ')
-            conn.send(data.encode()) # encode the string into byte for client
-        else:
-            break
+        print('waiting for connection')
+        conn, client_addr = sock.accept()
+        print(f'connection from {client_addr}')
 
-finally:
+        try:
+            while True:
+                data = conn.recv(1024).decode() # decode byte from client
+                if data:
+                    print('from client: ' + str(data))
+                    # data = input('=>=> ')
+                    data = 'data received'
+                    conn.send(data.encode()) # encode the string into byte for client
+                else:
+                    break
+        finally:
+            conn.close()
+            print('client left')
+except KeyboardInterrupt:
+    data = 'server is shutting down'
+    print(data)
+    # conn.send(data.encode())
     conn.close()
-    print('client left')
+
